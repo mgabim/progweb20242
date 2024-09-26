c19686ae3bf65e75b23a87f844272b1a0dd2dc36
mgabim <101949352+mgabim@users.noreply.github.com>
2024-09-26 12:25:48 -0300

    Add files via upload

---
+++ atv1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>atividade php nº1</title>
+</head>
+<body>
+    <?php
+    echo "<img src=https://picsum.photos/500/600>";
+    echo "<p>floresta bonita</p>";
+    ?>
+</body>
+</html>
