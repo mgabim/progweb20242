3a5a969d89fd5aa688c6e1d7a2c4fabe8c273ac6
mgabim <101949352+mgabim@users.noreply.github.com>
2024-09-26 12:44:43 -0300

    Add files via upload

---
+++ atv2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>atividade php nº2</title>
+</head>
+<body>
+    <?php
+    echo "<h1>URLs absolutas</h1>";
+    echo "<a href=\"http://google.com\">google</a>";
+    echo "<br>";
+    echo "<a href=\"http://youtube.com\">youtube</a>";
+    echo "<h1>URLs relativas</h1>";
+    echo "<a href=\"pagina1.html\">pagina 1</a>";
+    echo "<br>";
+    echo "<a href=\"pagina2.html\">pagina 2</a>";
+    ?>
+</body>
+</html>
